86b3b147b9a60420667ad8b2916940e70284bca5
Lars T Hansen <lhansen@adobe.com>
2009-01-26 15:28:18 -0800

    (rishah) Fix 475415 -  Consolidate basic data types defined in MMgc and avmplus to VMPI.h (r+lhansen)

--- vprof/vprof.h
+++ vprof/vprof.h
@@ -85,22 +85,8 @@
 #define THREADED 0
 #define THREAD_SAFE 0
 
-#ifdef _MSC_VER
-#ifndef __avmplus_types__
-typedef unsigned char      uint8_t;
-typedef unsigned short     uint16_t;
-typedef signed char        int8_t;
-typedef short              int16_t;
-typedef unsigned int       uint32_t;
-typedef signed int         int32_t;
-typedef __int64            int64_t;
-typedef unsigned __int64   uint64_t;
-typedef long long          int64_t;
-typedef unsigned long long uint64_t;
-#endif
-#else
-#include <inttypes.h>
-#endif
+#include "VMPI.h"
+
 
 // portable align macro
 #if defined(_MSC_VER)
