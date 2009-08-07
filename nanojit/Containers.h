4156cc37741caa0eec41e0b3b9d11a5b2b4c2dc2
Edwin Smith <edwsmith@adobe.com>
2009-08-07 06:55:25 -0400

    Convert CodegenLIR deadvars structures to Allocator, remove GC* (bug 508801 r=tharwood+)
    * This was the last use of GC* in CodegenLIR, removed.
    * Comments added to deadvars() explaining structures and lifetimes
    * added assert requiring nanojit::HashMap nbuckets > 0

--- nanojit/Containers.h
+++ nanojit/Containers.h
@@ -199,6 +199,7 @@ namespace nanojit
             , nbuckets(nbuckets)
             , buckets(new (a) Seq<Node>*[nbuckets])
         {
+            NanoAssert(nbuckets > 0);
             clear();
         }
 
