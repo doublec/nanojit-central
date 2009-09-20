b85f1b65970fa3af7dd82c968743e40814b57c88
Rick Reitmaier <rreitmai@adobe.com>
2009-09-20 11:40:04 -0700

    Bug 515138 - TM/nanojit: merge the two StackFilter passes.  r=gal. [nnethercote]

--- nanojit/Assembler.cpp
+++ nanojit/Assembler.cpp
@@ -760,8 +760,7 @@ namespace nanojit
         // Used for debug printing, if needed
         verbose_only(
         ReverseLister *pp_init = NULL;
-        ReverseLister *pp_after_sf1 = NULL;
-        ReverseLister *pp_after_sf2 = NULL;
+        ReverseLister *pp_after_sf = NULL;
         )
 
         // set up backwards pipeline: assembler -> StackFilter -> LirReader
@@ -780,32 +779,17 @@ namespace nanojit
         prev = pp_init;
         })
 
-        /*// STOREFILTER for sp
-        StackFilter storefilter1(prev, alloc, frag->lirbuf, frag->lirbuf->sp);
-        prev = &storefilter1;
-
-        verbose_only( if (_logc->lcbits & LC_AfterSF_SP) {
-        pp_after_sf1 = new (alloc) ReverseLister(prev, alloc, frag->lirbuf->names, _logc,
-                                         "After Storefilter(sp)");
-        prev = pp_after_sf1;
+#ifdef TM_MERGE
+        // STACKFILTER
+        StackFilter stackfilter(prev, alloc, frag->lirbuf, frag->lirbuf->sp, frag->lirbuf->rp);
+        prev = &stackfilter;
+#endif
+        
+        verbose_only( if (_logc->lcbits & LC_AfterSF) {
+        pp_after_sf = new (alloc) ReverseLister(prev, alloc, frag->lirbuf->names, _logc,
+                                                "After StackFilter");
+        prev = pp_after_sf;
         })
-
-        // STOREFILTER for rp
-        StackFilter storefilter2(prev, alloc, frag->lirbuf, frag->lirbuf->rp);
-        prev = &storefilter2;
-
-        verbose_only( if (_logc->lcbits & LC_AfterSF_RP) {
-        pp_after_sf2 = new (alloc) ReverseLister(prev, alloc, frag->lirbuf->names, _logc,
-                                         "After StoreFilter(rp) (final LIR)");
-        prev = pp_after_sf2;
-        })*/
-
-        // end of pipeline
-        verbose_only(
-        VerboseBlockReader vbr(alloc, prev, this, frag->lirbuf->names);
-        if (_logc->lcbits & LC_Assembly)
-            prev = &vbr;
-        )
 
         _inExit = false;
 
@@ -833,8 +817,7 @@ namespace nanojit
         // call finish() to emit whatever contents they have accumulated.
         verbose_only(
         if (pp_init)        pp_init->finish();
-        if (pp_after_sf1)   pp_after_sf1->finish();
-        if (pp_after_sf2)   pp_after_sf2->finish();
+        if (pp_after_sf)    pp_after_sf->finish();
         )
     }
 
