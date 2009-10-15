43b54b3c5e955d8e3931824aef11795666a8981c
Steven Johnson <stejohns@adobe.com>
2009-10-15 16:39:00 -0700

    nanojit/LIR.cpp: add missing ifdef FEATURE_NANOJIT (r=me)

--- nanojit/LIR.cpp
+++ nanojit/LIR.cpp
@@ -2136,6 +2136,7 @@ namespace nanojit
 #endif // NJ_VERBOSE
 
 
+#ifdef FEATURE_NANOJIT
 #ifdef DEBUG
     LIns* SanityFilter::ins1(LOpcode v, LIns* s0)
     {
@@ -2228,6 +2229,7 @@ namespace nanojit
         }
         return out->ins3(v, s0, s1, s2);
     }
+#endif
 #endif
 
 }
